a9eabfd183dbdeee92dcdb9c4346b39e8d4b58f6
Ryan Sperzel <ryansperzel@gmail.com>
2017-10-25 10:45:54 -0400

    review form post fetch

--- app/controllers/api/reviews_controller.rb
+++ app/controllers/api/reviews_controller.rb
@@ -1,6 +1,8 @@
 class Api::ReviewsController < ApplicationController
 
   def create
+    @review = Review.new(name: params["name"], body: params["body"], haunted_house_id: params["haunted_house_id"])
+    @review.save
   end
 
   def show
